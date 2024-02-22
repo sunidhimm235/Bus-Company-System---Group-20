9c69b33a916234f8a118b4a01454c9b7bbdfd363
Adam Elias <122506412+elias-adam-gh@users.noreply.github.com>
2024-02-21 19:59:03 -0500

    added auto incrementing userID function

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+const connection = mongoose.createConnection(process.env.DATABASE_URI);
+autoIncrement.initialize(connection);
+
 const userSchema = new mongoose.Schema({
   username: { type: String, 
     required: true, 
@@ -19,6 +22,13 @@ const userSchema = new mongoose.Schema({
     required: true, 
     enum: ['user', 'employee', 'admin'], default: 'user' 
   }
+});
+
+userSchema.plugin(autoIncrement.plugin, { 
+  model: 'User', 
+  field: 'userId', 
+  startAt: 1, 
+  incrementBy: 1
 });
 
 userSchema.pre('save', async function(next) {
