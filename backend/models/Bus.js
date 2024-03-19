f67806f73825505cc90821ff8e02caf0764d95e9
Adam Elias <122506412+elias-adam-gh@users.noreply.github.com>
2024-03-19 18:31:15 -0400

    Reconfig bus model schemas

--- backend/models/Bus.js
+++ backend/models/Bus.js
@@ -6,57 +6,82 @@ const validateTotalSeats = function (seats) {
     return totalSeats === 40;
 }
 
+const seatSchema = new Schema({
+    type: { 
+        type: String, 
+        required: true, 
+        enum: ['economy', 'premium', 'business'] 
+    },
+    seats: { 
+        type: [[Boolean]], 
+        required: true, 
+        default: false 
+    }
+});
+
+const routeSchema = new Schema({
+    startPoint: { 
+        type: String, 
+        required: true 
+    },
+    startTime: { 
+        type: String, 
+        required: true 
+    },
+    endPoint: { 
+        type: String, 
+        required: true 
+    },
+    endTime: { 
+        type: String, 
+        required: true 
+    }
+});
+
+const priceSchema = new Schema({
+    economy: { 
+        type: Number, 
+        required: true 
+    },
+    premium: { 
+        type: Number, 
+        required: true 
+    },
+    business: { 
+        type: Number, 
+        required: true 
+    }
+});
+
 const busSchema = new Schema({
-    busNumber: {type: Number,
-        required: true,
-        unique: true
+    busNumber: { 
+        type: Number, 
+        required: true, 
+        unique: true 
     },
-    busName: {type: String,
-        required: true
+    busName: { 
+        type: String, 
+        required: true 
     },
-    busDay: {type: String,
-        required: true
+    busDay: { 
+        type: String, 
+        required: true 
     },
-    activeStatus: {type: Boolean,
-        required: true,
-        default: true
+    activeStatus: { 
+        type: Boolean, 
+        required: true, 
+        default: true 
     },
-    seats: [{
-        economy: {type: [[Boolean]] ,
-            required: true,
-            default:  false,
-        },
-        premium: {type: [[Boolean]] ,
-            required: true,
-            default:  false,
-        },
-        business: {type: [[Boolean]] ,
-            required: true,
-            default:  false,
-        },
-        validate: [validateTotalSeats, 'Total seats should be 40']
-    }],
-    duration: {type: String,
-        required: true
+    seats: [seatSchema],
+    duration: { 
+        type: String, 
+        required: true 
     },
-    routes: [{startPoint: {type: String,
-            required: true},
-        startTime: {type: String,
-            required: true},
-        endPoint: {type: String,
-            required: true},
-        endTime: {type: String,
-            required: true}
-    }],
-    price: [{
-        economy: {type: Number,
-            required: true},
-        premium: {type: Number,
-            required: true},
-        business: {type: Number,
-            required: true}
-    }]
-})
+    routes: [routeSchema],
+    price: [priceSchema]
+});
+
+busSchema.path('seats').validate(validateTotalSeats, 'Total seats should be 40');
 
 const Bus = mongoose.model('Bus', busSchema);
 
