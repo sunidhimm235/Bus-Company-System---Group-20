d86ca60be4741cf31b0b3b63fb4c34ed79d65b22
LegomasterN <heikes.nicholas@gmail.com>
2024-03-23 14:11:45 -0400

    Faq/Contact Page

--- client/src/Components/ContactPage/ContactPage.jsx
+++ client/src/Components/ContactPage/ContactPage.jsx
@@ -1,20 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import './ContactPage.css'
+import Navbar from './/../NavBar/Navbar.jsx'; 
+import Footer from './/../Footer/Footer.jsx'; 
+import CityMap from '../../assets/CityMap.png';
 
-// Should be completed by Nick
+const ContactPage = (props) => {
+  const [Name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
 
-// This page should have the following:
-// Customer care: Help with cancellations, refunds, and bookings
-// Feedback of the app
-// Contact information of the bus reservation system (dummy values)
-// Contact form for the user to fill out
-// Contact information we need from the user: Email, Phone number, Address
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
 
-// Styling for this page should be on the ContactPage.css file
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const mess =  {Name, email, phone, message}
+    console.log(mess)
+    setErrorMessage('Error not fully implemented yet');
+    /*
+    try {
+      await axios.post('http://localhost:4000/contactRoutes/createRoute', {fName, lName, email, phone, message});
+      navigate('/home');
+    } catch (error) {
+      setErrorMessage(error.response.data.message || 'Error contacting support');
+    }
+            style={{ backgroundImage: `url(${CityMap})` }}
 
-function ContactPage() {
+    */
+    
+  };
+
   return (
-    <div>ContactPage</div>
+    <div>
+      <Navbar />
+      <div className='background'>
+      </div>
+      <div className="contact-page-container">
+        <div className="contact-page-holder">
+          <div className="contact-page-cp-message">
+            <h1 className="contact-page-heading large-text">Get in Touch</h1>
+              <div className="contact-info">
+                <div>
+                  <span>Looking to reach out to us? Feel free to give us a call, send an email, or fill out the contact form below.</span>
+                </div>
+                <div>
+                  <span>---</span>
+                </div>
+                <div>
+                  <span>+234 123 456 7890</span>
+                </div>
+                <div>
+                  <span>contactUs@ticketride.com</span>
+                </div>
+            </div>
+          </div>
+          <div className="cp-form-container">
+            {errorMessage && <div className="error-message" style={{ color: 'red', marginBottom: '10px' }}>{errorMessage}</div>}
+            <form className="contactForm" onSubmit={handleSubmit}>
+              <span className="cp-name-text">Name</span>
+              <input
+                type="text"
+                placeholder="Name"
+                required={true}
+                value={Name}
+                onChange={(e) => setName(e.target.value)}
+                className="cp-name-in"
+              />
+              <span className="cp-email-text">Email Address</span>
+              <input
+                type="text"
+                placeholder="Email Address"
+                required={true}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="cp-mail-in"
+              />
+              <span className="cp-phone-text">Phone Number</span>
+              <input
+                type="text"
+                placeholder="XXX-XXX-XXXX"
+                required={true}
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                className="cp-phone-in"
+              />
+              <span className="cp-q-text"> How can we assist you today? </span>
+              <textarea
+                placeholder="Questions, Comments, Concerns"
+                required={true}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                className="cp-q-in"
+              ></textarea>
+              <button type="submit" className="cp-button">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
--- client/src/Components/FAQPage/FAQPage.jsx
+++ client/src/Components/FAQPage/FAQPage.jsx
@@ -1,19 +1,83 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './FAQPage.css'
+import Navbar from './/../NavBar/Navbar.jsx'; 
+import Footer from './/../Footer/Footer.jsx'; 
 
-// Should be completed by Nick
 
-// This page should have the following:
-// - Frequently asked questions
-// - Questions like "How to book a ticket?", "How to cancel a ticket?" etc.
-// - Answers to the questions
-// - Contact information of the bus reservation system (dummy values)
+const FAQPage = (props) => 
+{
+  const [openAccordions, setOpenAccordions] = useState([]); // All accordions open by default
 
-// Styling for this page should be on the FAQPage.css file
+  const faqData = [
+    {
+      question: 'Q. How early should I arrive before departure?',
+      answer: 'A. We recommend arriving at least 30 minutes before the scheduled departure time to allow enough time for boarding and any necessary procedures.',
+    },
+    {
+      question: 'Q. How can I book a ticket?',
+      answer: 'A. Easily book your tickets through our user-friendly website by choosing your destination, travel date, and preferred time.',
+    },
+    {
+      question: 'Q. How can I cancel a ticket?',
+      answer: 'A. Easily cancel your tickets through our website.',
+    },
+    {
+      question: 'Q. What is your luggage policy?',
+      answer: 'A. Each passenger is allowed one carry-on bag and one checked bag free of charge. Additional baggage may incur extra fees.',
+    },
+    {
+      question: 'Q. What payment methods do you accept?',
+      answer: 'A. We accept all major credit cards (Visa, Mastercard, American Express, Discover), as well as PayPal and Apple Pay.',
+    },
+    {
+      question: 'Q. Are pets allowed on board?',
+      answer: 'A. We are happy to inform you that pets are allowed on board.',
+    },
+    {
+      question: 'Q. What are your safety measures amid COVID-19?',
+      answer: 'A. We have implemented various safety measures in accordance with local health guidelines, including mandatory mask-wearing, regular sanitization of buses, and limited seating capacity to ensure social distancing.',
+    },
+    {
+      question: 'Q. Can I get a refund if I cancel my ticket?',
+      answer: 'A. Refund policies vary depending on the type of ticket and the timing of the cancellation. Please contact our customer service team for any questions or concerns.',
+    },
+    {
+      question: 'Q. Do you offer Wi-Fi on your buses?',
+      answer: 'A. Yes, we provide complimentary Wi-Fi on board our buses, allowing you to stay connected during your journey.',
+    },
+  ];
 
-function FAQPage() {
+  const toggleAccordion = (index) => {
+    setOpenAccordions((prevOpenAccordions) =>
+      prevOpenAccordions.includes(index)
+        ? prevOpenAccordions.filter((i) => i !== index)
+        : [...prevOpenAccordions, index]
+    );
+  };
+
   return (
-    <div>FAQPage</div>
+    
+    <div className="background-container">
+      <div> 
+      <Navbar />
+      <div className="faq-page-container">
+        <h1 className="faq-page-text">Frequently Asked Questions</h1>
+        <div className="accordion-container">
+          {faqData.map((item, index) => (
+            <div className="accordion-item" key={index}>
+              <div className="accordion-header" onClick={() => toggleAccordion(index)}>
+                <h3>{item.question}</h3>
+              </div>
+              {openAccordions.includes(index) && (
+                <div className="accordion-content">{item.answer}</div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />  
+    </div>
+  </div>
   )
 }
 
