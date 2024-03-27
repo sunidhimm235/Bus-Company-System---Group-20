2e270fca2cca2516c574b47e25a19a25eb601d38
Adam Elias <122506412+elias-adam-gh@users.noreply.github.com>
2024-03-27 15:20:17 -0400

    Setup alt user dashb & config routes to user featr

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/NavBar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 import Info from './Components/Info/Info';
-import Navbar from './Components/NavBar/Navbar';
 import Search from './Components/Search/Search';
 import Subscribe from './Components/Subscribe/Subcribe';
-import SignInPage from './Components/SignInPage/SignInPage'; // Adjust path as necessary
-import SignUpPage from './Components/SignUpPage/SignUpPage'; // Adjust path as necessary
+import SignInPage from './Components/SignInPage/SignInPage';
+import SignUpPage from './Components/SignUpPage/SignUpPage';
 import Admin from './Components/Admin/Admin';
 import BusSchedule from './Components/BusSchedule/BusSchedule';
 import TravelInformation from './Components/TravelInformation/TravelInformation';
@@ -16,56 +16,41 @@ import FAQPage from './Components/FAQPage/FAQPage';
 import ContactPage from './Components/ContactPage/ContactPage';
 import SeatSelection from './Components/BusSchedule/SeatSelection';
 import UserInformation from './Components/UserInformation/UserInformation';
+import TravelHistory from './Components/TravelHistory/TravelHistory'; // Ensure this path is correct
 import './index.css';
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <div>
         <Routes>
           <Route path="/" element={
             <>
-              <Navbar/>
               <Home />
-              <Search/>
-              <Info/>
-              <Subscribe/>
-              <Footer/>
+              <Search />
+              <Info />
+              <Subscribe />
+              <Footer />
             </>
           } />
           <Route path="/sign-in" element={<SignInPage />} />
           <Route path="/sign-up" element={<SignUpPage />} />
           <Route path="/admin" element={<Admin />} />
-          <Route path="/bus-schedule" element={
-              <>
-                <Navbar />
-                <BusSchedule />
-              </>
-          } />
-          <Route path='/seat-selection' element={
-            <>
-              <Navbar />
-              <SeatSelection />
-            </>
-          } />
-          <Route path='/user-information' element={
-            <>
-              {/* <Navbar /> */}
-              <UserInformation />
-            </>
-          } />
-          <Route path='/travel-information' element={<TravelInformation />} />
-          <Route path='/destination' element={<DestinationPage />} />
-          <Route path='/faq' element={<FAQPage />} />
-          <Route path='/contact' element={<ContactPage />} />
-          {/* Add more routes here if you have other pages */}
+          <Route path="/bus-schedule" element={<BusSchedule />} />
+          <Route path="/seat-selection" element={<SeatSelection />} />
+          <Route path="/user-information" element={<UserInformation />} />
+          <Route path="/travel-information" element={<TravelInformation />} />
+          <Route path="/destination" element={<DestinationPage />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/travel-history" element={<TravelHistory />} />
+          {/* Add more routes as needed */}
         </Routes>
-        {/* Components that should be available in all routes can stay outside Routes */}
-        {/* Example: <Navbar/> and potentially <Footer/> if you want it on every page */}
       </div>
+      {}
     </Router>
   );
-}
+};
 
 export default App;
-
